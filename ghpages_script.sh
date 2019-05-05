0c30f0f6e51428e348e4b6e9cc42549fa1cf4dfc
Arnaud Legrand <arnaud.legrand@imag.fr>
2019-05-05 22:50:11 +0200

    [CI] Fixing travis-CI ?

--- ghpages_script.sh
+++ ghpages_script.sh
@@ -2,10 +2,10 @@
 # git config --global user.email "you@example.com"
 # git config --global user.name "Your Name"
 
-# # clone the repository to the book-output directory
-# git clone -b gh-pages \
-#   https://alegrand@github.com/alegrand/bookrr.git \
-#   book-output
+# clone the repository to the book-output directory
+git clone --depth=0 -b gh-pages \
+  https://alegrand@github.com/alegrand/bookrr.git \
+  book-output
 make all
 cd book-output
 git rm -rf *
