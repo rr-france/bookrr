29ac87d345dd9958e34f261faa5965185b6743ed
Arnaud Legrand <arnaud.legrand@imag.fr>
2019-05-06 16:33:03 +0200

    Use perl instead of sed to make a better job with bib processing.

---
+++ bib-fix.pl
@@ -0,0 +1,77 @@
+#!/usr/bin/perl -w
+use strict;
+
+# open(INPUT,"refs.bib");
+
+my($bib_head, $bib_body, $bib_isbn, $bib_url, $bib_year, $bib_urldate);
+
+sub reset_bib {
+    $bib_body = $bib_head = $bib_url = $bib_isbn = $bib_year = $bib_urldate = "";
+}
+
+reset_bib();
+
+while(defined(my $line=<>)) {
+    chomp($line);
+    if($line =~ /^@/){
+	$line =~ s/,\s*//g;
+	$bib_head = $line;
+	$bib_body = $bib_head;
+	next;
+    } 
+    if($bib_head eq "") { next; }
+
+    if($line =~ /^\s*note = \s*{(.*)}/) {
+	next;
+    }    	
+    if($line =~ /^\s*url = \s*{(.*)}/) {
+	$bib_url = $1;
+	$bib_url = "URL~: \\url{$bib_url}";
+	next;
+    }
+    if($line =~ /^\s*isbn = \s*{(.*)}/) {
+	$bib_isbn = $1;
+	$bib_isbn = "ISBN~: $bib_isbn";
+	next;
+    }
+    if($line =~ /^\s*year = \s*{(.*)}/) {
+	$bib_year = $1;
+	next;
+    }
+    if($line =~ /^\s*urldate = \s*{(.*)}/) {
+	$bib_urldate = $1;
+	$bib_urldate =~ s/-.*//g;
+	next;
+    }
+
+    if($line =~/^}$/) {
+	print $bib_body;
+
+	my $bib_suffix = "";
+	
+	#  note (URLS + ISBN)
+	my $bib_note = $bib_url;
+	if($bib_isbn ne "") {
+	    if($bib_note ne "") {
+		$bib_note .= ". ".$bib_isbn;
+	    } else {
+		$bib_note = $bib_isbn;
+	    }
+	}
+	if($bib_note ne "") {$bib_suffix = "    note = {$bib_note},\n"; }
+	#  year
+	if($bib_year eq "") { $bib_year = $bib_urldate; }
+	if($bib_year ne "") { $bib_suffix .= "    year = {$bib_year}\n"; }
+
+	if($bib_suffix ne "") {
+	    print ",\n".$bib_suffix;
+	}
+	
+	print "}\n";
+	reset_bib();
+	next;
+    }
+
+    $line =~ s/\s*,\s*$//g;
+    $bib_body .= ",\n".$line;
+}
