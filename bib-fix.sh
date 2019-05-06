58851ed4f0636b662acc324ab07651cbc52c4545
Arnaud Legrand <arnaud.legrand@imag.fr>
2019-05-06 17:10:10 +0200

    Fix my bibliography  perl script ?

--- bib-fix.sh
+++ bib-fix.sh
@@ -2,5 +2,5 @@ sed -i 's|url = {//www.fun-mooc.fr/|url = {https://www.fun-mooc.fr/|g' refs.bib
 sed -i 's|« |«~|g' refs.bib
 sed -i 's| » |~»|g' refs.bib
 mv refs.bib orefs.bib
-bib-fix.pl < orefs.bib > refs.bib
+perl bib-fix.pl < orefs.bib > refs.bib
 rm orefs.bib
